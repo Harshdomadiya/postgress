d146627c32e129dbfde0b59970a13f521ccac7dd
Harshdomadiya <125807302+Harshdomadiya@users.noreply.github.com>
2024-03-28 12:09:58 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://ahzlvkzv:BnC61OGncbLoWw8s5REg8-U1Pu3W494w@flora.db.elephantsql.com/ahzlvkzv");
+    const client = new Client("DATABASE-URL");
     await client.connect();
     console.log("database connection established");
     return client;
